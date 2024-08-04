c35a21a7e544a6b597da106e2bb0e68cf6d7cbc8
Ly Gia Bao <69019508+baorlys@users.noreply.github.com>
2024-08-04 20:02:47 +0700

    fix HomeController

--- src/main/java/org/example/microsoftlists/controller/HomeController.java
+++ src/main/java/org/example/microsoftlists/controller/HomeController.java
@@ -1,7 +1,7 @@
 package org.example.microsoftlists.controller;
 import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -9,7 +9,7 @@ import java.io.IOException;
 @RestController
 @Hidden
 public class HomeController {
-    @RequestMapping("/")
+    @GetMapping("/")
     public void home(HttpServletResponse response) throws IOException {
         response.sendRedirect("/swagger-ui/index.html");
     }
